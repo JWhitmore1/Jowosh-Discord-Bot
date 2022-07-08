54921bd1493747abd783cd2b40b18c869f9a5c6f
Christopher <spinklesponkle@gmail.com>
2022-07-09 01:47:32 +1000

    say and banner commands added

--- bot.py
+++ bot.py
@@ -49,5 +49,42 @@ async def test(ctx):
 
     await ctx.respond(embed)
 
+@bot.command
+@lightbulb.option('text', 'What will Jowosh say', type=str)
+@lightbulb.command('say', 'Speak vicariously through Jowosh')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def say(ctx):
+    await ctx.respond(ctx.options.text)
+
+@bot.command
+@lightbulb.option('text', 'What will Jowosh say', type=str)
+@lightbulb.command('banner', 'Speak through Jowosh, clearly!')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def say(ctx):
+    message = ''
+    conv = {'0': 'zero',
+            '1': 'one',
+            '2': 'two',
+            '3': 'three',
+            '4': 'four',
+            '5': 'five',
+            '6': 'six',
+            '7': 'seven',
+            '8': 'eight',
+            '9': 'nine'}
+
+    for char in ctx.options.text:
+        if char in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ':
+            message += f':regional_indicator_{char.lower()}: '
+        elif char in '0123456789':
+            num = conv[char]
+            message += f':{num}: '
+        elif char == ' ':
+            message += '   '
+        else:
+            message += char
+
+    await ctx.respond(message)
+
 bot.run()
 
