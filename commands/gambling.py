0d990339c1e9715f3be7e1290908ae32982f5d87
JWhitmore1 <joshua.whitmore09@gmail.com>
2022-07-10 21:13:24 +1000

    added coinfip command

--- commands/gambling.py
+++ commands/gambling.py
@@ -14,9 +14,22 @@ async def ball(ctx):
     await ctx.respond(ctx.options.question + '\n :8ball:  |  ' + random.choice(options_8ball))
 
 
+@lightbulb.command('coinflip', 'do you get head or tail!?')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def flip(ctx):
+    result = random.randint(0, 1)
+    if result == 1:
+        await ctx.respond(":coin: | You flipped Heads!")
+    else:
+        await ctx.respond(":coin: | You flipped Tails!")
+
+
 def load(bot: lightbulb.BotApp):
     bot.command(ball)
+    bot.command(flip)
 
 
 def unload(bot: lightbulb.BotApp):
     bot.remove_command(bot.get_slash_command("ball"))
+    bot.remove_command(bot.get_slash_command("flip"))
+
