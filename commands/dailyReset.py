f12846fb0ed24da03162676b22be676ea4b38c03
JWhitmore1 <38652089+JWhitmore1@users.noreply.github.com>
2022-08-05 14:13:02 +1000

    Revert "economy"
    This reverts commit 69c3f62c41f28f4d03b0a424dcb0c4aca2b3217b.

--- commands/dailyReset.py
+++ commands/dailyReset.py
@@ -2,10 +2,9 @@ import lightbulb
 from datetime import datetime
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
-from db import get_db
 
 
-reset_time = "0600"
+reset_time = 6
 daily_plugin = lightbulb.Plugin("Daily")
 sched = AsyncIOScheduler()
 sched.start()
@@ -13,11 +12,8 @@ sched.start()
 
 @sched.scheduled_job(CronTrigger(minute="*/1"))
 async def daily_reset() -> None:
-    if datetime.strftime(datetime.now(),"%I%M") == reset_time:
-        print("resetting")
+    if int(datetime.strftime(datetime.now(),"%I")) == reset_time:
         # reset all users dayclaim and bankclaim
-        db = get_db()
-        db.execute("UPDATE economy SET dayclaim = 0, bankclaim = 0")
         pass
         # await daily_plugin.app.rest.create_message(995308487011737662, "Resetting fr")
 
--- db.py
+++ db.py
@@ -14,11 +14,8 @@ def initialise():
     schema = f.read()
     f.close()
 
-    conn.executescript(schema)
+    conn.execute(schema)
 
-def check_id(id, db):
-    if db.execute('SELECT * FROM economy WHERE ID = ?', (id,)).fetchone() is None:
-        db.execute('INSERT INTO economy (ID) VALUES (?)', (id,))
 
 def check_pair(sender, recipient, db):
     pair_id = sender + recipient
