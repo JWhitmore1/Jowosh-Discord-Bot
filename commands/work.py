4171b5992de0988ffbca4492c422426b0bc3c9b8
JWhitmore1 <joshua.whitmore09@gmail.com>
2022-08-31 21:44:41 +1000

    basic math questions

--- commands/work.py
+++ commands/work.py
@@ -1,4 +1,4 @@
-from doctest import REPORT_CDIFF
+from msilib.schema import Component
 import hikari
 import lightbulb
 import random
@@ -9,65 +9,53 @@ from db import get_db, check_econ_id
 plugin = lightbulb.Plugin("Work")
 
 
+def build_row(bot, options, answer):
+    row = bot.rest.build_action_row()
+    for i in range(len(options)):
+        if options[i] == answer:
+            button = row.add_button(hikari.ButtonStyle.PRIMARY, f"answer")
+        else:
+            button = row.add_button(hikari.ButtonStyle.PRIMARY, f"option_{i}")
+        button.set_label(str(options[i]))
+        button.add_to_container()
+    return row
+
+
 @plugin.command()
 @lightbulb.option('level', 'what level of math would you like to complete', type=int, min_value=1, max_value=4)
-@lightbulb.command('math-work', 'earn gold by doing maths!')
+@lightbulb.command('math', 'earn gold by doing maths!')
 @lightbulb.implements(lightbulb.SlashCommand)
 async def math(ctx):
     db = get_db()
     id = ctx.member.id
-    channel = ctx.channel_id
     check_econ_id(id, db)
 
     if ctx.options.level == 1:
         numbers = [random.randint(1, 49), random.randint(1, 49)]
         operator = random.choice(["+", "-"])
         
-        if operator == "+": answer = numbers[0] + numbers[1]
-        if operator == "-": answer = numbers[0] - numbers[1]
-        
-        await ctx.respond(f"{numbers[0]} {operator} {numbers[1]} = ?")
+        if operator == "+": 
+            answer = numbers[0] + numbers[1]
+        if operator == "-": 
+            answer = numbers[0] - numbers[1]
 
-        timeout = time.time() + 10
+        ans_options = [answer, answer + random.randint(1, 5), answer - random.randint(1, 5), answer + random.randint(5, 10)]
+        random.shuffle(ans_options)
 
-        def check(m):
-            return m.content is not None and m.channel == channel
-
-        response = await hikari.impl.GatewayBot.wait_for(hikari.impl.GatewayBot, hikari.MessageCreateEvent, timeout=20)
-        print(response)
-
-        # response = await plugin.listener(hikari.MessageCreateEvent, responseListener(id, channel))
-        # while time.time() < timeout:
-        #     if response != None:
-        #         break
-            
-        if response == None:
-            await ctx.respond("you did not reply in time")
-        else:
-            content = response.content
-            print(content)
-
-            if content == answer:
-                await ctx.respond("Correct")
-            else:
-                await ctx.respond("Incorrect")
+        buttons = build_row(ctx.bot, ans_options, answer)
+        await ctx.respond(f"{numbers[0]} {operator} {numbers[1]} = ", component=buttons)
 
 
-# @plugin.listener(hikari.MessageCreateEvent)
-async def responseListener(event, id, channel):
-    try:
-        if event.author_id == id and event.channel_id == channel:
-            print("message recieved")
-            return(event)
-    except TypeError:
-        print(event.content)
-        
+@plugin.listener(hikari.InteractionCreateEvent)
+async def on_component_interaction(event: hikari.InteractionCreateEvent) -> None:
+    # Filter out all unwanted interactions
+    if not isinstance(event.interaction, hikari.ComponentInteraction):
+        return
 
-@plugin.command()
-@lightbulb.command('stop', 'stop task')
-@lightbulb.implements(lightbulb.SlashCommand)
-async def stop(ctx):
-    ctx.respond("stop, please!")
+    if event.interaction.custom_id == "answer":
+        await event.interaction.create_initial_response(hikari.ResponseType.MESSAGE_CREATE, 'Correct :thumbsup:')
+    else:
+        await event.interaction.create_initial_response(hikari.ResponseType.MESSAGE_CREATE, 'dumbass')
 
 
 def load(bot: lightbulb.BotApp):
