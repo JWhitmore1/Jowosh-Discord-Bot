9fd06aaf0a5c341029114fcd2cb37850ca98d797
stackotter <stackotter@stackotter.dev>
2022-09-04 10:42:15 +1000

    Send a new problem after a previous maths problem is completed

--- commands/work.py
+++ commands/work.py
@@ -13,14 +13,14 @@ plugin = lightbulb.Plugin("Work")
 maths_server_base_url = os.environ.get("maths_problem_gen_api_url", "http://localhost:8000")
 
 
-def build_row(bot, choice_count, answer):
-    row = bot.rest.build_action_row()
+def build_row(rest, choice_count, answer, level):
+    row = rest.build_action_row()
     letters = ["a", "b", "c", "d"]
     for i in range(choice_count):
         if i == answer:
-            button = row.add_button(hikari.ButtonStyle.PRIMARY, f"answer")
+            button = row.add_button(hikari.ButtonStyle.PRIMARY, f"answer_{level}")
         else:
-            button = row.add_button(hikari.ButtonStyle.PRIMARY, f"choice_{i}")
+            button = row.add_button(hikari.ButtonStyle.PRIMARY, f"choice_{i}_{level}")
         button.set_label(letters[i])
         button.add_to_container()
     return row
@@ -35,9 +35,21 @@ def fetch_question(level):
     urls = [hikari.files.URL(f"http://localhost:8000{url}") for url in [problem_url] + choice_urls]
     return [urls, choice_urls, answer]
 
+async def send_problem(respond, level, rest):
+    try:
+        response = json.loads(requests.get(f"{maths_server_base_url}/rand-problem?level={level}").text)
+        problem_url = response["problem_url"]
+        answer = response["answer"]
+        choice_urls = response["choice_urls"]
+
+        urls = [hikari.files.URL(f"{maths_server_base_url}{endpoint}") for endpoint in [problem_url] + choice_urls]
+        buttons = build_row(rest, len(choice_urls), answer, level)
+        await respond("", attachments=urls, component=buttons)
+    except Exception as e:
+        await respond(f"Failed to load problem: {e}")
 
 @plugin.command()
-@lightbulb.option('level', 'what level of math would you like to complete', type=int, min_value=1, max_value=3, default=1)
+@lightbulb.option('level', 'what level of math would you like to complete', type=int, min_value=1, max_value=2, default=1)
 @lightbulb.command('math', 'earn gold by doing maths!')
 @lightbulb.implements(lightbulb.SlashCommand)
 async def math(ctx):
@@ -47,19 +59,7 @@ async def math(ctx):
 
     response = await ctx.respond("Loading...")
     message = await response.message()
-
-    try:
-        level = ctx.options.level
-        response = json.loads(requests.get(f"{maths_server_base_url}/rand-problem?level={level}").text)
-        problem_url = response["problem_url"]
-        answer = response["answer"]
-        choice_urls = response["choice_urls"]
-
-        urls = [hikari.files.URL(f"{maths_server_base_url}{endpoint}") for endpoint in [problem_url] + choice_urls]
-        buttons = build_row(ctx.bot, len(choice_urls), answer)
-        await message.edit("", attachments=urls, component=buttons)
-    except Exception as e:
-        await message.edit(f"Failed to load problem: {e}")
+    await send_problem(message.edit, ctx.options.level, ctx.bot.rest)
 
 
 @plugin.listener(hikari.InteractionCreateEvent)
@@ -68,10 +68,17 @@ async def on_component_interaction(event: hikari.InteractionCreateEvent) -> None
     if not isinstance(event.interaction, hikari.ComponentInteraction):
         return
 
-    if event.interaction.custom_id == "answer":
-        await event.interaction.create_initial_response(hikari.ResponseType.MESSAGE_CREATE, 'Correct :thumbsup:')
+    button_id = event.interaction.custom_id
+    if button_id.startswith("answer_"):
+        level = int(button_id.split("_")[-1])
+        await event.interaction.create_initial_response(hikari.ResponseType.MESSAGE_CREATE, 'Correct :thumbsup: Loading next problem...')
+        await send_problem(event.interaction.execute, level, event.interaction.app.rest)
+        await event.interaction.edit_initial_response('Correct :thumbsup:')
     elif event.interaction.custom_id.startswith("choice_"):
-        await event.interaction.create_initial_response(hikari.ResponseType.MESSAGE_CREATE, 'dumbass')
+        level = int(button_id.split("_")[-1])
+        await event.interaction.create_initial_response(hikari.ResponseType.MESSAGE_CREATE, 'dumbass. Loading next problem...')
+        await send_problem(event.interaction.execute, level, event.interaction.app.rest)
+        await event.interaction.edit_initial_response('dumbass')
 
 
 def load(bot: lightbulb.BotApp):
